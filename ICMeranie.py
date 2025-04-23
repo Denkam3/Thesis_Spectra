ca9e0a2cf8074d785d60710ba8399da0c8ea8299
Denkam3 <164533612+Denkam3@users.noreply.github.com>
2025-04-23 23:49:41 +0200

    Add files via upload

---
+++ ICMeranie.py
@@ -0,0 +1,54 @@
+from brokenaxes import brokenaxes
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
+import pandas as pd
+
+# Načítanie dát pre jednotlivé čiary
+file_paths = ["Jungle Juice_MIR.csv", "Amsterdam_MIR.csv", "Rave_MIR.csv"]
+offsets = [0, 0.2, 0.4]  # Posuny pre jednotlivé dataset-y
+data_list = []
+
+for file_path, offset in zip(file_paths, offsets):
+    data = pd.read_csv(file_path, sep = ";", header = None )
+    x, y = data.iloc[:, 0], data.iloc[:, 1] + offset
+    data_list.append((x, y))
+
+# Vytvorenie grafu s prerušením osi pomocou brokenaxes
+fig = plt.figure(figsize=(10, 6))
+bax = brokenaxes(xlims=((500, 1800), (2800, 3050)), hspace=0.05)  # Nastavenie prerušenia osi x
+
+# Vykreslenie čiar
+labels = ["Jungle Juice", "Amsterdam", "Rave"]
+for (x, y), label in zip(data_list, labels):
+    bax.plot(x, y, linestyle='-', linewidth=2, label= label)
+
+# Nastavenie popisov
+bax.set_xlabel(r'Vlnočet ($\text{cm}^{-1}$)', fontsize=20, labelpad=40)
+bax.set_ylabel("Relatívna absorbancia", fontsize=20, labelpad = 5)
+bax.legend(fontsize=20, loc='upper right')
+
+# Nastavenie hlavných a vedľajších čiark na os x pre ľavú časť grafu (bax.axs[0])
+bax.axs[0].xaxis.set_major_locator(MultipleLocator(150))  # Hlavné čiarky každých 200 jednotiek
+bax.axs[0].xaxis.set_minor_locator(MultipleLocator(50))   # Vedľajšie čiarky každých 50 jednotiek
+
+# Nastavenie hlavných a vedľajších čiark na os x pre pravú časť grafu (bax.axs[1])
+bax.axs[1].xaxis.set_major_locator(MultipleLocator(150))  # Hlavné čiarky každých 200 jednotiek
+bax.axs[1].xaxis.set_minor_locator(MultipleLocator(50))   # Vedľajšie čiarky každých 50 jednotiek
+
+# Prispôsobenie vzhľadu čiark a odstránenie čísel a čiark na osi y
+bax.axs[0].tick_params(axis='y', labelleft=False, length=0)  # Odstránenie čísel a čiark na osi y pre ľavú časť grafu
+bax.axs[1].tick_params(axis='y', labelleft=False, length=0)  # Odstránenie čísel a čiark na osi y pre pravú časť grafu
+
+# Prispôsobenie vzhľadu čiark na osi x
+bax.axs[0].tick_params(axis='x', which='major', length=10, width=1.5, labelsize=20)  # Hlavné čiarky
+bax.axs[0].tick_params(axis='x', which='minor', length=5, width=1, labelsize=12)     # Vedľajšie čiarky
+
+bax.axs[1].tick_params(axis='x', which='major', length=10, width=1.5, labelsize=20)  # Hlavné čiarky
+bax.axs[1].tick_params(axis='x', which='minor', length=5, width=1, labelsize=12)     # Vedľajšie čiarky
+
+bax.axs[0].set_ylim(-0.04, 0.8)
+bax.axs[1].set_ylim(-0.04, 0.8)
+
+plt.tight_layout()
+plt.show()
