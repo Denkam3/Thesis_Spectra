cdb2ebe0ab3b3b87ded306ff7b2ed3e3d2401155
Denkam3 <164533612+Denkam3@users.noreply.github.com>
2025-10-06 01:04:16 +0200

    Update RVypoc.py

--- RVypoc.py
+++ RVypoc.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 
 def filter_data(data, threshold=0):
+    """Odfiltruj ľavú stranu"""
     return data[data[:, 0] >= threshold]
 
 def remove_x_range(data, low=1850, high=2800):
     """Odstráni dáta v intervale x od 'low' po 'high'."""
     return data[(data[:, 0] < low) | (data[:, 0] > high)]
 
-# Načítanie dát pre prvú čiaru
 file_path = "pvTz_Mol1_Ram"
 data = np.loadtxt(file_path)
 # data = filter_data(data)
@@ -18,7 +18,6 @@ data = np.loadtxt(file_path)
 
 x, y = data[:, 0], data[:, 1]
 
-# Načítanie dát pre druhú čiaru
 file_path_2 = "pvTz_Mol2_Ram"
 data_2 = np.loadtxt(file_path_2)
 # data_2 = filter_data(data_2)
@@ -47,33 +46,30 @@ data_5 = np.loadtxt(file_path_5)
 
 x5, y5 = data_5[:, 0], data_5[:, 1]
 
-# Vytvorenie grafu s prerušením osi pomocou brokenaxes
+# Vytvorenie grafu s prerušením osi
 fig = plt.figure(figsize=(10, 6))
-bax = brokenaxes(xlims=((0, 1800), (2800, 3500)), hspace=0.05)  # Nastavenie prerušenia osi x
+bax = brokenaxes(xlims=((0, 1800), (2800, 3500)), hspace=0.05)
 
-# Vykreslenie grafu
+# Vykresli
 bax.plot(x, y, linestyle='-', linewidth=2, label="1")
 bax.plot(x2, y2, linestyle='-', linewidth=2, label="2")
 bax.plot(x3, y3, linestyle='-', linewidth=2, label="3")
 bax.plot(x4, y4, linestyle='-', linewidth=2, label="4")
 bax.plot(x5, y5, linestyle='-', linewidth=2, label="5")
 
-# Nastavenie popisov
+# POPIS
 bax.set_xlabel(r'Ramanov posun ($\text{cm}^{-1}$)', fontsize=20, labelpad=40)
 bax.set_ylabel("Intenzita", fontsize=20, labelpad = 5)
 bax.legend(fontsize=20, loc='upper right')
 
-# Nastavenie hlavných a vedľajších čiark na os x pre ľavú časť grafu (bax.axs[0])
-bax.axs[0].xaxis.set_major_locator(MultipleLocator(200))  # Hlavné čiarky každých 200 jednotiek
-bax.axs[0].xaxis.set_minor_locator(MultipleLocator(50))   # Vedľajšie čiarky každých 50 jednotiek
+bax.axs[0].xaxis.set_major_locator(MultipleLocator(200))
+bax.axs[0].xaxis.set_minor_locator(MultipleLocator(50))
 
-# Nastavenie hlavných a vedľajších čiark na os x pre pravú časť grafu (bax.axs[1])
-bax.axs[1].xaxis.set_major_locator(MultipleLocator(200))  # Hlavné čiarky každých 200 jednotiek
-bax.axs[1].xaxis.set_minor_locator(MultipleLocator(50))   # Vedľajšie čiarky každých 50 jednotiek
+bax.axs[1].xaxis.set_major_locator(MultipleLocator(200)) 
+bax.axs[1].xaxis.set_minor_locator(MultipleLocator(50))
 
-# Prispôsobenie vzhľadu čiark a odstránenie čísel a čiark na osi y
-bax.axs[0].tick_params(axis='y', labelleft=False, length=0)  # Odstránenie čísel a čiark na osi y pre ľavú časť grafu
-bax.axs[1].tick_params(axis='y', labelleft=False, length=0)  # Odstránenie čísel a čiark na osi y pre pravú časť grafu
+bax.axs[0].tick_params(axis='y', labelleft=False, length=0) 
+bax.axs[1].tick_params(axis='y', labelleft=False, length=0)  
 
 # Prispôsobenie vzhľadu čiark na osi x
 bax.axs[0].tick_params(axis='x', which='major', length=10, width=1.5, labelsize=20)  # Hlavné čiarky
@@ -83,4 +79,5 @@ bax.axs[1].tick_params(axis='x', which='major', length=10, width=1.5, labelsize=
 bax.axs[1].tick_params(axis='x', which='minor', length=5, width=1, labelsize=12)     # Vedľajšie čiarky
 
 plt.tight_layout()
+
 plt.show()
